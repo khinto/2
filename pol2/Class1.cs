0dcca5c41620e45f4d4f74c632631baac5f16df0
khinto <bachana.khintibidze.1@btu.edu.ge>
2020-10-04 23:23:44 -0700

    second

---
+++ pol2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pol2
+{
+    class Class1
+    {
+        public int Mandzili(int tevadoba, int mandzili)
+        {
+            return tevadoba * mandzili;
+        }
+
+        public double Mandzili(double max_sichqare,double dro)
+        {
+            return max_sichqare * dro;
+        }
+    }
+}
---
+++ pol2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pol2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 obj_1 = new Class1();
+            int tevadoba, mandzili,ganvlili_mandzili;
+            double max_sichqare, dro,mandzili_1;
+
+
+            tevadoba = int.Parse(textBox1.Text);
+            mandzili = int.Parse(textBox2.Text);
+            max_sichqare = double.Parse(textBox3.Text);
+            dro = double.Parse(textBox4.Text);
+
+
+            ganvlili_mandzili = obj_1.Mandzili(tevadoba,mandzili);
+            mandzili_1 = obj_1.Mandzili(max_sichqare, dro);
+
+            label5.Text = "ავტომობილი სავსე ბაკით გაივლის " + ganvlili_mandzili.ToString()+ " კმ";
+            label6.Text = "ავტომობილი გაივლის " + mandzili_1.ToString() + " კმ";
+
+        }
+    }
+}
